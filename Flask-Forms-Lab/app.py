f3ea6b8a6e2d82b62607fed9db9475b35052829e
leeorta23-meet <109899549+leeorta23-meet@users.noreply.github.com>
2022-08-01 10:27:17 +0300

    slay

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -4,19 +4,39 @@ import random
 app = Flask(  # Create a flask app
 	__name__,
 	template_folder='templates',  # Name of html file folder
-	static_folder='static'  # Name of directory for static files
+	static_folder='static'  # Name of directory fopr static files
 )
 
 
-username = "llo2ay"
+username = "leeor"
 password = "123"
 facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
 
 
-@app.route('/')  # '/' for the default page
+
+@app.route('/', methods=['GET', 'POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
+	if request.method == 'GET':
+		return render_template('login.html')
+
+	else: 
+		username = request.form['username']
+		password = request.form['password']
+		return redirect(url_for('home'))
   
+  
+@app.route('/home', methods=['GET', 'POST'])  # '/' for the default page
+def home():
+  if request.method == 'GET':
+  	return render_template('home.html', facebook_friends = facebook_friends)
+
+
+
+@app.route('/friends_exists/<string:name>', methods=['GET', 'POST'])  # '/' for the default page
+def friend_exist(name):
+  if request.method == 'GET':
+  	return render_template('friend_exists.html', name = name, friends = facebook_friends)
+
 
 
 
